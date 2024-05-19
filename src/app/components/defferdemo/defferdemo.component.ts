88999055d34577476235667a4bc456297d21c8cc
Nikhil Muneshwar <93129551+NikhilM01@users.noreply.github.com>
2024-05-19 18:44:24 +0530

    Add files via upload

---
+++ src/app/components/defferdemo/defferdemo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defferdemo',
+  standalone: true,
+  imports: [],
+  templateUrl: './defferdemo.component.html',
+  styleUrl: './defferdemo.component.css'
+})
+export class DefferdemoComponent {
+
+}
