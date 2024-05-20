bb79d825e434e5446ac212877ae8900ddbd1fb4b
NikhilM01 <nikhilmuneshwar01@gmail.com>
2024-05-20 17:22:54 +0530

    dark mode

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive, NgIf, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   constructor(public authService:AuthService){}
+  isCollapsed = false;
+
+    
 }
