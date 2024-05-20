f52c1a3decf23663d92ce8cbd6725d6e2f0ec71f
Nikhil Muneshwar <93129551+NikhilM01@users.noreply.github.com>
2024-05-20 12:14:49 +0530

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTYe5xsIxzdZzO-2yaC43tC8XNXjBUuIQ",
+    authDomain: "codebin-nik.firebaseapp.com",
+    projectId: "codebin-nik",
+    storageBucket: "codebin-nik.appspot.com",
+    messagingSenderId: "116460178044",
+    appId: "1:116460178044:web:25743b4b8d5973319d17f9"
+  };
